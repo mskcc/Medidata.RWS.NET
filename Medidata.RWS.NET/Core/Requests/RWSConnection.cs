8a05d55e0545fb0de6ca98ef1a13001b8b831dd5
Matt <matthewjameskoch@gmail.com>
2017-05-05 13:03:31 -0400

    Add project files.

---
+++ Medidata.RWS.NET/Core/Requests/RWSConnection.cs
@@ -0,0 +1,9 @@
+﻿namespace Medidata.RWS.NET.Core.Requests
+{
+    /// <summary>
+    ///  Represents a connection to RAVE Web Services.
+    /// </summary>
+    public class RwsConnection
+    {
+    }
+}
